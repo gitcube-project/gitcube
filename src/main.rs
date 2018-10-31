750a32554abf1cbeabe2d2aac6ad24d9d64d94db
JunWang <1103870790@qq.com>
2018-11-01 00:09:28 +0800

    session support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::collections::HashMap;
+
 extern crate actix_web;
-use actix_web::{App, http::Method, fs, server, HttpRequest, HttpResponse};
+use actix_web::{App, Form, http::Method, fs, server, HttpRequest, HttpResponse};
+use actix_web::middleware::session::{RequestSession, SessionStorage, CookieSessionBackend};
+
 
 extern crate regex;
 use regex::Regex;
@@ -7,7 +11,7 @@ use regex::Regex;
 #[macro_use]
 extern crate tera;
 use tera::Tera;
-// use tera::Context;
+use tera::Context;
 
 #[macro_use]
 extern crate serde_json;
@@ -24,9 +28,7 @@ lazy_static! {
     };
 }
 
-
-
-fn index(req: &HttpRequest) -> HttpResponse {
+fn index(_req: &HttpRequest) -> HttpResponse {
     let contents = TERA.render("index.html", &json!({
         "name": "John Doe"
     })).unwrap();
@@ -35,27 +37,29 @@ fn index(req: &HttpRequest) -> HttpResponse {
         .body(&contents)
 }
 
-fn signin_page(req: &HttpRequest) -> HttpResponse {
-    let contents = TERA.render("signin.html", &json!({
-        "name": "John Doe"
-    })).unwrap();
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(&contents)
+fn signin_page(_req: &HttpRequest) -> HttpResponse {
+    let contents = TERA.render("signin.html", &json!({})).unwrap();
+    HttpResponse::Ok().body(&contents)
+}
+
+fn signin_action((req, form): (HttpRequest, Form<HashMap<String, String>>)) -> HttpResponse {
+    if form.contains_key("email") && form.contains_key("password"){
+        // check in db
+
+        // if ok save in session
+        let mut context = Context::new();
+        context.insert("email", &form["email"]);
+        let contents = TERA.render("signin.html", &context).unwrap();
+        req.session().set("context", context).unwrap();
+        HttpResponse::Ok().body(&contents)
+    }else{
+        HttpResponse::Ok().body("error parameters")
+    }
+    
 }
 
 
-fn signin_action(req: &HttpRequest) -> HttpResponse {
-    let contents = TERA.render("signin.html", &json!({
-        "name": "John Doe"
-    })).unwrap();
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(&contents)
-}
-
-
-fn signup(req: &HttpRequest) -> HttpResponse {
+fn signup(_req: &HttpRequest) -> HttpResponse {
     let contents = TERA.render("signup.html", &json!({
         "name": "John Doe"
     })).unwrap();
@@ -99,15 +103,11 @@ fn profile(req: &HttpRequest) -> HttpResponse {
 }
 
 fn main() {
-    server::new(|| {vec![
-        App::new()
-            .prefix("/user")
-            .resource("/signin", |r|{
-                r.method(Method::GET).f(signin_page);
-                r.method(Method::POST).f(signin_action);
-            })
-            .resource("/signup", |r| r.f(signup)),
-        App::new()
+    server::new(|| App::new()
+            .middleware(SessionStorage::new(
+                CookieSessionBackend::signed(&[0; 32]).secure(false)
+            ))
+            .resource("", |r| r.f(index))
             .resource("/", |r| r.f(index))
             .handler("/logo",
             fs::StaticFiles::new("public/logo").unwrap().show_files_listing())
@@ -117,8 +117,13 @@ fn main() {
             fs::StaticFiles::new("public/assets").unwrap().show_files_listing())
             .handler("/css",
             fs::StaticFiles::new("public/assets").unwrap().show_files_listing())
-            .resource("/{name:[0-9a-zA-Z]+}", |r| r.f(profile)),
-    ]})
+            .resource("/{name:[0-9a-zA-Z]+}", |r| r.f(profile))
+            .resource("/user/signin", |r|{
+                r.method(Method::GET).f(signin_page);
+                r.method(Method::POST).with(signin_action)
+            })
+            .resource("/user/signup", |r| r.f(signup))
+    )
     .bind("127.0.0.1:8088")
     .unwrap()
     .run();
